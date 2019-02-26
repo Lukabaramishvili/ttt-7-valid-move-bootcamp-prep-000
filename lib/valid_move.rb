a783ceaa3bef0a3d7f0a395a59ca0c26b6d1d2fa
Luka Baramishvili <lukabaramishvili@gmail.com>
2019-02-26 15:50:13 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,21 +2,25 @@
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
-# def position_taken(board, index)
-#   if board[index] == " " || board[index] == "" || board[index] == nil
-#     return false
-#   elsif board[index] == "X" || board[index] == "O"
-#     return true
-#   end
-# end
-
-def valid_move?(board, index)
-  if board[index] == " " || board[index] == "" || board[index] == nil && index.between?(0, 8)
+def position_taken(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  elsif board[index] == "X" || board[index] == "O"
     return true
-  elsif board[index] == "X" || board[index] == "O" && index.between?(0 , 8)
-    return false
-  else
-    return false
   end
 end
+
+def valid_move?(board, index)
+  index.between?(0,8) && !position_taken(board, index)
+end 
+
+# def valid_move?(board, index)
+#   if board[index] == " " || board[index] == "" || board[index] == nil && index.between?(0, 8)
+#     return true
+#   elsif board[index] == "X" || board[index] == "O" && index.between?(0 , 8)
+#     return false
+#   else
+#     return false
+#   end
+# end
 
